01bc5688dee9d1f41c9e985bb6cbd40ff9d103b2
RealestCapybara <owenwray1@gmail.com>
2023-02-19 15:00:15 +0000

    Added more comments and documentation, and a type check in Quat initialisation

--- Main.py
+++ Main.py
@@ -2,13 +2,21 @@ import numpy as np
 from numpy.linalg import norm
 
 class Quat():
+    """Quaternion object. Requires 4 components, 1 real, 3 imaginary.
+    Vector Form: q = 0 + x i + y j + z k
+    Rotation Form: q = cos(A/2) + sin(A/2)[n_x i + n_y j + n_z k]
+    """
     def __init__(self, q0, q1, q2, q3):
-        self.q0 = q0
-        self.q1 = q1
-        self.q2 = q2
-        self.q3 = q3
+        try:
+            self.q0 = np.float64(q0)
+            self.q1 = np.float64(q1)
+            self.q2 = np.float64(q2)
+            self.q3 = np.float64(q3)
+        except:
+            raise TypeError("Quaternion can only take numeric input data")
 
     def __mul__(self, other):
+        """Defines Quaternion self multiplication and scaling."""
         if type(other) == type(self):
             q0 = self.q0
             q1 = self.q1
@@ -33,12 +41,14 @@ Quaternion cannot be multiplied with object of type {type(other)}\n\
 Can only be multiplied by another quaternion or scaled")
 
     def __rmul__(self, other):
+        """Defined in case a scaler is on the other side of the product"""
         if type(other) == type(self):
             return other.__mul__(self)
         else:
             return self.__mul__(other)
 
     def __add__(self, other):
+        """Defines the termwise addition of Quaternions"""
         if type(other) == type(self):
             return Quat(self.q0+other.q0, self.q1+other.q1,
                          self.q2+other.q2, self.q3+other.q3)
@@ -75,9 +85,16 @@ Quaternion cannot be summed with non-Quaternion object")
             yield val
 
     def conjugate(self):
+        """Returns the conjugate of the Quaterion object"""
         return Quat(self.q0, -self.q1, self.q2, -self.q3)
 
 class Pend():
+    """Pendulum Object.
+    mass (numeric) default=1.0 Encodes mass (both inertial and gravitational)
+    pos (Quat) default=Quat(0, 0, 0, 1) Encodes length and orientation
+    vel (Quat) default=Quat(0, 0, 0, 0) Encodes velocity
+    acc (Quat) default=Quat(0, 0, 0, 0) Encodes acceleration
+    """
     def __init__(self, mass=1.0, pos = Quat(0, 0, 0, 1),
                  vel = Quat(0, 0, 0, 0), acc = Quat(0, 0, 0, 0)):
         """Pendulum Object"""
@@ -93,19 +110,36 @@ class Pend():
         self.mass = mass
 
     def rotate(self, delta):
+        """Rotates the position by a given Delta (change in position)
+        Needs very small delta due to use of trig approximations"""
         pos = self.pos
+        #for two "vector form" quaternions, their product is analogous to a 
+        #cross product. Therefore by taking the quaternion product of the 
+        #position and the delta, a new quaternion parallel to the axis of 
+        #rotation and equal to the product of the radius and the angular 
+        #component of delta
         q = self.pos * delta
 
         posA = abs(pos)
         qA = abs(q)
 
+        #to turn q into a rotation quaternion, the angle can be calculated
+        #by using the tan(theta)=theta approximation, which results in 
+        #theta = |q|/(|pos|^2) which can then be used to scale q by 
+        #sin(theta/2)/|q| (1/|q| to account for the necessary normalisation)
+        #and added to cos(theta/2). Quadratic trig approximations are used for
+        #both. As the delta is expected to be small, their use is tolerable.
+        #This means that errors will be expected to scale cubicly.
         q = q * (1/(2*posA**2))
         q = q + Quat(1-(qA**2/(8*posA**4)), 0, 0, 0)
+
+        #once the rotation quaternion is calculated, all that is needed to
+        #apply it is simple quaternion multiplication
         self.pos = q * pos * q.conjugate()
 
 if __name__ == "__main__":
     X = Pend(pos=Quat(0, 0, 0, 10))
-    Delta = Quat(0, 0.01, 0.01, 0.0)
+    Delta = Quat(0, 0.1, 0.1, 0.0)
     X.rotate(Delta)
     print(X.pos)
     print(abs(X.pos))
