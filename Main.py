5cf73f5ba476ef83f23b3000bd09f2d00f01eaa3
RealestCapybara <owenwray1@gmail.com>
2023-03-02 15:04:16 +0000

    Added functions to generate matrices for generating differential equations

--- Main.py
+++ Main.py
@@ -29,7 +29,7 @@ class Pendulum():
 
         self.zPolar = bool(zPolar)
 
-    def diffTheta(self):
+    def dT(self):
         l = self.pos[0]
         theta = self.pos[1]
         phi = self.pos[2]
@@ -43,7 +43,7 @@ class Pendulum():
                              l*np.cos(theta)*np.cos(phi),
                              l*np.cos(theta)*np.sin(phi)])
 
-    def diffPhi(self):
+    def dP(self):
         l = self.pos[0]
         theta = self.pos[1]
         phi = self.pos[2]
@@ -56,7 +56,7 @@ class Pendulum():
             return np.array([0,
                              -l*np.sin(theta)*np.sin(phi),
                              l*np.sin(theta)*np.cos(phi)])
-    def diffTheta2(self):
+    def dT2(self):
         l = self.pos[0]
         theta = self.pos[1]
         phi = self.pos[2]
@@ -70,7 +70,7 @@ class Pendulum():
                              -l*np.sin(theta)*np.cos(phi),
                              -l*np.sin(theta)*np.sin(phi)])
 
-    def diffPhi2(self):
+    def dP2(self):
         l = self.pos[0]
         theta = self.pos[1]
         phi = self.pos[2]
@@ -84,7 +84,7 @@ class Pendulum():
                              -l*np.sin(theta)*np.cos(phi),
                              -l*np.sin(theta)*np.sin(phi)])
 
-    def diffThetaPhi(self):
+    def dTP(self):
         l = self.pos[0]
         theta = self.pos[1]
         phi = self.pos[2]
@@ -112,7 +112,7 @@ class Pendulum():
 
         thetadot = deepcopy(self.vel[0])
         phidot = deepcopy(self.vel[1])
-        v = thetadot * self.diffTheta() + phidot * self.diffPhi()
+        v = thetadot * self.dT() + phidot * self.dP()
 
         if self.zPolar:
             phi2 = np.arctan2(-cosTheta, sinTheta * sinPhi)
@@ -126,8 +126,8 @@ class Pendulum():
         self.pos[1] = theta2
         self.pos[2] = phi2
 
-        self.vel[0] = (1/self.pos[0])**2 * v * self.diffTheta
-        self.vel[1] = (1/(self.pos[0] * np.sin(theta2)))**2 *v*self.diffPhi()
+        self.vel[0] = (1/self.pos[0])**2 * v * self.dT()
+        self.vel[1] = (1/(self.pos[0] * np.sin(theta2)))**2 *v*self.dP()
 
         self.zPolar = not self.zPolar
 
@@ -143,5 +143,39 @@ class Pendulum():
             return np.array([l*np.cos(theta),
                              l*np.sin(theta)*np.cos(phi),
                              l*np.sin(theta),np.sin(phi)])
+class Mat():
+    @staticmethod
+    def AElement(q1, q2):
+        if type(q1) is not Pendulum or type(q2) is not Pendulum:
+            raise TypeError("arguments must be of type Pendulum")
+        return np.array([[q2.dT()@q1.dT(), q2.dP()@q1.dT()],
+                         [q2.dT()@q1.dP(), q2.dP()@q1.dP()]])
+    @staticmethod
+    def BElement(q1, q2):
+        if type(q1) is not Pendulum or type(q2) is not Pendulum:
+            raise TypeError("arguments must be of type Pendulum")
+        return np.array([[q2.dT2()@q1.dT(),q2.dTP()@q1.dT(),q2.dP2()@q1.dT(),
+                          q2.dT2()@q1.dP(),q2.dTP()@q1.dP(),q2.dP2()@q1.dP()]])
+    @staticmethod
+    def Matrix(qList, Type):
+        MassList = [q.mass for q in qList]
+        array = []
+        if Type == 'A':
+            func = Mat.AElement
+        elif Type == 'B':
+            func = Mat.BElement
+        else:
+            raise ValueError("Type needs to be 'A' or 'B'")
+        for i, vi in enumerate(qList):
+            row = []
+            for j, vj in enumerate(qList):
+                Element = func(vi,vj) * sum(MassList[max(i, j):])
+                row.append(Element)
+            array.append(row)
+        return np.block(array)
 
-
+if __name__ == '__main__':
+    q1 = Pendulum(pos=[10, 0, 0], vel=[0,0], acc=[0,0], mass=1)
+    q2 = Pendulum(pos=[10, 0, 0], vel=[0,0], acc=[0,0], mass=1)
+    q3 = Pendulum(pos=[10, np.pi, 0], vel=[0,0], acc=[0,0], mass=1)
+    print(Mat.Matrix([q1,q2, q3], 'B'))
